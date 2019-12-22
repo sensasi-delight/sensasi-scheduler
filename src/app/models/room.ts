00410768f2889f6bd4e0fc8af78407d206b757f7
Zain Adam <zainadam.id@gmail.com>
2019-12-22 21:18:09 +0800

    added: crud for room

--- src/app/models/room.ts
+++ src/app/models/room.ts
@@ -1,2 +1,50 @@
+import { environment } from 'src/environments/environment';
+
+
 export class Room {
+  id: string;
+  name: string;
+  _type: number;
+  static types: Array<string> = [
+    'Ruang Kelas',
+    'Lab'
+  ]
+
+  static uniqueKeys = [
+    'id', 'name'
+  ];
+
+  static data = (() => {
+    return Room.getData();
+  })();
+
+  static refreshData() {
+    Room.data = Room.getData();
+  };
+
+  private static getData() {
+    let result = [];
+    const local = JSON.parse(localStorage.getItem(environment.localStorageItemName));
+
+    if (local && local['rooms']) {
+      local['rooms'].forEach(element => {
+        let temp = new Room;
+        Object.assign(temp, element);
+        result.push(temp);
+      });
+    }
+
+    return result;
+  }
+
+  static toLocalStorage() {
+    let local = JSON.parse(localStorage.getItem(environment.localStorageItemName));
+    local['rooms'] = Room.data;
+    localStorage.setItem(environment.localStorageItemName, JSON.stringify(local));
+  }
+
+  //GETTER
+  get type(): string {
+    return Room.types[this._type];
+  }
 }
--- src/app/pages/room/room.component.ts
+++ src/app/pages/room/room.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from 'src/app/models/room';
 
 @Component({
   selector: 'app-room',
@@ -6,10 +7,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room.component.css']
 })
 export class RoomComponent implements OnInit {
+  RoomClass = Room;
+  model: Room = new Room;
+  storeMode: string;
+  rooms = Room.data;
+  workingIndex: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    $('[data-toggle="tooltip"]').tooltip();
   }
 
+  initCreate() {
+    this.model = new Room;
+    this.storeMode = 'create';
+    $('#roomFormModal').modal('show');
+  }
+
+  initDelete(index): void {
+    this.storeMode = 'delete';
+    this.workingIndex = index;
+    Object.assign(this.model, {...this.rooms[index]});
+    $('#roomDeleteModal').modal('show');
+  }
+
+  initUpdate(index): void {
+    this.storeMode = 'update';
+    this.workingIndex = index;
+    Object.assign(this.model, {...this.rooms[index]});
+    $('#roomFormModal').modal('show');
+  }
+
+  storeRoom(formData): void {
+    this.checkUnique(formData);
+
+    if (formData.form.valid) {
+      switch (this.storeMode) {
+        case 'create':
+          let newRoom = new Room;
+          Object.assign(newRoom, {...this.model})
+          this.rooms.push(newRoom);
+
+          break;
+
+        case 'update':
+          Object.assign(this.rooms[this.workingIndex], {...this.model});
+          break;
+
+        default:
+          break;
+      }
+
+      $('#roomFormModal').modal('hide');
+      formData.resetForm();
+      Room.toLocalStorage();
+    }
+  }
+
+  deleteRoom(): void {
+    $('#roomDeleteModal').modal('hide');
+    this.rooms.splice(this.workingIndex, 1);
+    Room.toLocalStorage();
+  }
+
+  private checkUnique(formData) {
+    for (const key in formData.form.controls) {
+      this.rooms.forEach((room, index) => {
+        if (Room.uniqueKeys.indexOf(key) != -1) {
+          if (room[key] == formData.form.controls[key].value) {
+            if (this.storeMode == 'create' || (this.storeMode == 'update' && this.workingIndex != index)) {
+              formData.form.controls[key].setErrors({unique: true})
+            }
+          }
+        }
+      });
+    }
+  }
 }
